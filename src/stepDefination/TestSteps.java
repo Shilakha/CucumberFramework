d190c0431ace944cad08d66da57018183719cf12
Shilakha <shilakha.shilakha@evry.com>
2017-12-11 15:43:31 +0530

    bb

--- src/stepDefination/TestSteps.java
+++ src/stepDefination/TestSteps.java
@@ -1,12 +1,17 @@
 package stepDefination;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 
@@ -51,45 +56,33 @@ public class TestSteps {
 		// Write code here that turns the phrase above into concrete actions
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		WebElement e = driver.findElement(By.xpath("//input[@class='LM6RPg']"));
-		e.sendKeys("clothing");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		e.sendKeys("laptops");
 		driver.findElement(By.xpath("//button[@class='vh79eN']")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		
-		 driver.findElement(By.xpath("//div[@class='_3BTv9X']")).click();
-		//driver.findElement(By.cssSelector("._3BTv9X")).click();
-		// driver.findElement(By.xpath(".//*[@id='swatch-3-size']/a")).click();
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title='Search for products, brands and more']")));
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@title='HP']//div[@class='_1p7h2j']")));
+		Thread.sleep(1000);
 		 JavascriptExecutor jse = (JavascriptExecutor)driver;
-			//for scrolling to bottom
-			jse.executeScript("window.scrollBy(0,250)", "");
-		
+		 jse.executeScript("window.scrollBy(0,250)", "");
+		 driver.findElement(By.xpath("//div[@title='HP']//div[@class='_1p7h2j']")).click();
+		 //driver.findElement(By.xpath("//*[@data-tracking-id='Top Brands_Samsung']")).click();
+		Thread.sleep(5000);
+		//driver.findElement(By.xpath("//li[text()='Price -- High to Low']")).click();
+		List<WebElement> element = driver.findElements(By.xpath("//div[@class='_1vC4OE _2rQ-NK']"));
 
+		element.get(0).click();
 		for (String winHandle : driver.getWindowHandles()) {
 			driver.switchTo().window(winHandle);
 		}
-		
-		jse.executeScript("window.scrollBy(0,250)", "");
-		jse.executeScript("scroll(0, -250);");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		//jse.executeScript("window.scrollBy(0,250)", "");
-		driver.findElement(By.xpath("//*[@id='swatch-2-size']/a")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK']")).click();
 		
-		//jse.executeScript("scroll(0, -250);");
-		
-		//for left scrolling
-		//jse.executeScript("window.scrollBy(-2000,0)","");
 		
 		
-		//for right scrolling
-		//jse.executeScript("window.scrollBy(2000,0)","");
 		
 		
-		//for scrolling to upward
-		//		jse.executeScript("window.scrollBy(0,-250)", "");
-		//		OR,
-		//		jse.executeScript("scroll(0, -250);");
+		
 	}
-
 }
