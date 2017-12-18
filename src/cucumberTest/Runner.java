af8bc994f56b01891a30556442a92caf3840eb06
Shilakha <shilakha.shilakha@evry.com>
2017-12-18 15:23:35 +0530

    h

--- src/cucumberTest/Runner.java
+++ src/cucumberTest/Runner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\shilakha\\eclipse-workspace\\Flipkart\\Feature\\Feature.feature", glue = {
-		"C:\\Users\\shilakha\\eclipse-workspace\\Flipkart\\src\\stepDefination\\TestSteps.java" })
+		"C:\\Users\\shilakha\\eclipse-workspace\\Flipkart\\src\\stepDefination\\TestSteps.java" },plugin = {"pretty", "html:target/cucumber"})
 
 public class Runner {
 
