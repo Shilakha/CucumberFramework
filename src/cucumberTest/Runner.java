a1fec3bd7f3ffd40a4323adc3f88c2d4bb711016
Shilakha <shilakha.shilakha@evry.com>
2017-12-20 10:21:08 +0530

    g

--- src/cucumberTest/Runner.java
+++ src/cucumberTest/Runner.java
@@ -1,14 +1,11 @@
 package cucumberTest;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = {"src/Feature.feature"}, 
-glue = {"src/stepDefination" },
-plugin ={"json:target/cucumber.json"})
+@CucumberOptions(features = { "Features" }, glue = { "stepDefinition" })
 
 public class Runner {
 
